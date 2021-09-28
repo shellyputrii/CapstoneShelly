84004628f7fc7113cd1cc898338ac66c410fc37c
Shelly Putri <shellzaholic27@gmail.com>
2021-09-28 23:03:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/materialme/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.android.materialme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        int SPLASH_SCREEN_TIME = 2000; //5 DETIK
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(intent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_SCREEN_TIME);
+    }
+}
